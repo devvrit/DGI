640c7bd8aca286f7e61a3acc3492c4a97f1a9b33
Devvrit <f20150053@goa.bits-pilani.ac.in>
2022-01-01 08:48:43 +0530

    .

--- execute.py
+++ execute.py
@@ -13,13 +13,13 @@ dataset = 'ogbn-arxiv'
 
 # training params
 #batch_size = 2708
-batch_size = round(169343//2.7)
+# batch_size = round(169343//2.7)
 nb_epochs = 1000
-patience = 20
+patience = 25
 lr = 0.0001
 l2_coef = 0.0
 drop_prob = 0.0
-hid_units = 256
+hid_units = [256,256]
 sparse = True
 nonlinearity = 'prelu' # special name to separate parameters
 
@@ -30,7 +30,7 @@ features, _ = process.preprocess_features(features)
 nb_nodes = features.shape[0]
 ft_size = features.shape[1]
 nb_classes = labels.shape[-1]
-#batch_size = nb_nodes//64
+batch_size = round(nb_nodes//2.7)
 
 labels = torch.FloatTensor(labels[np.newaxis])
 idx_train = torch.LongTensor(idx_train)
--- models/dgi.py
+++ models/dgi.py
@@ -3,30 +3,45 @@ import torch.nn as nn
 from layers import GCN, AvgReadout, Discriminator
 
 class GCN_new(nn.Module):
-    def __init__(self, inp_dim, hidden_dims, act):
+    def __init__(self, inp_dim, hidden_dims, act, dropout=0.0):
         """
         :param inp_dim = dimension of X matrix representing node features
         :param hidden_dims = hidden layer dimensions
         """
         super(GCN_new, self).__init__()
+
+        assert len(hidden_dims)>0
+        self.Wr=[]
+        self.g = []
         self.hidden_dims = hidden_dims
+        self.num_layers = len(hidden_dims)
         self.inp_dim=inp_dim
-        self.Wr = nn.Linear(inp_dim, hidden_dims, bias=True)
-        self.W = nn.Linear(hidden_dims, hidden_dims, bias=True)
-        #torch.manual_seed(786)
-        nn.init.xavier_uniform_(self.Wr.weight.data)
-        nn.init.xavier_uniform_(self.W.weight.data)
-        self.Wr.bias.data.fill_(0.0)
-        self.W.bias.data.fill_(0.0)
-        self.g = nn.PReLU() if act == 'prelu' else nn.ReLU()
-        self.act = nn.PReLU() if act == 'prelu' else nn.ReLU()
+        self.Wr.append(nn.Linear(inp_dim, hidden_dims[0], bias=True))
+        nn.init.xavier_uniform_(self.Wr[0].weight)
+        self.g.append(nn.PReLU() if act == 'prelu' else act)
+        for i in range(1, self.num_layers):
+            self.Wr.append(nn.Linear(hidden_dims[i-1], hidden_dims[i], bias=True))
+            nn.init.xavier_uniform_(self.Wr[-1].weight)
+            self.g.append(nn.PReLU() if act == 'prelu' else act)
+        self.Wr = nn.ModuleList(self.Wr)
+        self.g = nn.ModuleList(self.g)
+        self.dropout=nn.Dropout(dropout)
         #print("self.Wr.weight.size() is: " + str(self.Wr.weight.size()))
         #print("self.Wr.weight is: " + str(self.Wr.weight))
         #print("self.Wr.bias is: " + str(self.Wr.bias))
 
     def forward(self, A, AX):
-        temp = torch.sparse.mm(A, self.g(self.Wr(AX)))
-        return torch.unsqueeze(self.act(self.W(temp)), 0)
+        temp1 = self.g[0](self.Wr[0](AX))
+        temp = self.dropout(temp1)
+        for i in range(1, self.num_layers):
+            temp1 = self.Wr[i](temp)
+            temp1 = torch.sparse.mm(A, temp1)
+            temp1 = self.g[i](temp1)
+            temp1 = self.dropout(temp1)
+            temp=temp1
+        return temp
+        # temp = torch.sparse.mm(A, self.g(self.Wr(AX)))
+        # return torch.unsqueeze(self.act(self.W(temp)), 0)
         #return torch.unsqueeze(self.g(self.Wr(AX)), 0)
         #return torch.unsqueeze(self.act(self.W(self.g(self.Wr(AX)))),0)
 
